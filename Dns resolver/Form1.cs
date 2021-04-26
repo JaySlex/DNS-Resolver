8331e76a681992ce50e07cc50b39ce32eb54f44e
JaySlex <iles.belkessam@gmail.com>
2021-04-26 11:34:02 -0400

    first commit

---
+++ Dns resolver/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Dns_resolver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            try
+            {
+                IPAddress[] ips = Dns.GetHostAddresses(textBox1.Text);
+                foreach (IPAddress item in ips)
+                {
+                    textBox2.Text += "\n"+item.MapToIPv4().ToString();
+                }
+            }
+            catch(Exception ex)
+            {
+                textBox2.Text = ex.Message;
+            }
+        }
+    }
+}
